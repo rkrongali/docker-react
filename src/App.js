8546cbebe129e6bd34a4b606cd261a8fbafd483f
ravi <rongali.ravi@gmail.com>
2020-01-16 13:11:14 -0600

    updated one

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, changed one.
         </a>
       </header>
     </div>
